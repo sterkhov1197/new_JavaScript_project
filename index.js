4e5916b59af6ee9bc2862483c3fbb38cb4e15717
sterkhov1197 <alexeysterkhov1997@gmail.com>
2024-05-10 19:29:52 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+/*
+let myName
+console.log(myName)
+
+
+const newObject={
+    name:"Alex",
+    age:25,
+    isAdult:true
+}
+
+const CopyOfObject=newObject;
+
+CopyOfObject.nameStreet="Levanevskoho";
+CopyOfObject.numStreet=87;
+
+console.log(newObject.nameStreet,newObject.numStreet);*/
+
+let variable ="Alex";
+variable = 12;
+console.log(variable);
